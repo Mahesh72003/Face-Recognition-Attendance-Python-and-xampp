9d25461a5a20e76bdd483da752505c5372057385
Mahesh A.V <130894034+Mahesh72003@users.noreply.github.com>
2025-02-17 22:00:51 +0530

    Delete face_recognition_attendance_python directory

--- face_recognition_attendance_python/open_whatappweb.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-def open_website(url):
-    webbrowser.open(url)
-
-if __name__ == "__main__":
-    website_url = "https://web.whatsapp.com/"  # Replace with the URL you want to open
-    open_website(website_url)
